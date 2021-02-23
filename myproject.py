685cf2d1177a483aa40675404fb3d91e2be64419
indifox <indifox6@gmail.com>
2021-02-23 16:30:08 +0300

    changes for redis BD

--- myproject.py
+++ myproject.py
@@ -2,27 +2,19 @@ from flask import Flask, jsonify, request , render_template
 from datetime import datetime
 import logging
 import redis
-import time 
 
 app = Flask(__name__)
-r = redis.Redis(host='172.17.0.2', port=6379)
+#my_redis - name of docker container
+r = redis.Redis(host='my_redis', port=6379)
 
 @app.route("/")
 def index():
-	"""logging.debug('This is a debug message')
-	logging.info('This is an info message')
-	logging.warning('This is a warning message')
-	logging.error('This is an error message')
-	logging.critical('This is a critical message')
-
-	return render_template("/index.html")"""
-	#Размер базы должен быть меньше 2^32-1;
-	print(r.sadd("nums", "1","2"))
-	#print(r.sismember("nums", "1"))
-	#print(r.scard("nums"))
-	#print(r.sadd("nums", "4"))
-	#print(r.smembers("nums"))
-	return 'Hello World! I have been seen {} times.\n'.format(r.smembers("nums"))
+	logging.info("The index page is opened!")
+	try:
+		return render_template('/index.html')
+	except:
+		data = dict(status = "Error" , datetime=str(datetime.now()), info='Some trouble with main page')
+		return jsonify(data)
 
 @app.route("/api/n", methods=['POST'])
 def api_number():
@@ -31,69 +23,46 @@ def api_number():
 		n = int(data['number'])
 		return api_number_par(n)
 	except:
-		data = dict(status = "Error" , number=n, datetime=str(datetime.now()), info='Some trouble with request')
+		data = dict(status = "Error" , number=n, datetime=str(datetime.now()), info='Some trouble with request or format')
 		return jsonify(data)
 
 @app.route("/api/n/<int:n>")
 def api_number_par(n):
 	now_time = str(datetime.now())
-	print("HIIII")
 	try:
-		if n<0:
-			raise ValueError()
 		if(r.sismember("nums",str(n))):
 			data = dict(status = "Error" , number=n, datetime=now_time, info='Number in DB')
-			#logging.error("Number {} in DB.\n".format(n))
+			logging.error("Number {} in DB.\n".format(n))
 			return jsonify(data)
 		elif(r.sismember("nums", str(n+1))):
 			data = dict(status = "Error" , number=n, datetime=now_time, info='Next number in DB')
-			#logging.error("Next number {} in DB.\n".format(n))
+			logging.error("Next number {} in DB.\n".format(n))
 			return jsonify(data)
 		else:
 			r.sadd("nums",str(n))
-			#logging.info("Number {} is added".format(n))
+			logging.info("Number {} is added".format(n))
 			data = dict(status = "Ok", number = n+1, datetime = now_time)
 			return jsonify(data)
-	except ValueError:
-		data = dict(status = "Error" , number=n, datetime=str(datetime.now()), info='The input number less then 0')
+	except:
+		data = dict(status = "Error" , number=n, datetime=now_time, info='Somethings happens with writing to BD or exceptions.')
 		return jsonify(data)
-	except:
-		return jsonify({'status': "Error"})
-
-@app.route("/api/log",methods=['GET'])
-def api_log():
-	"""
-	response = []
-	try:
-		for l in list_log:
-			response.append(dict(id = 0, number=l[1], datetime=l[2], info=l[3]))
-		return(jsonify(response))
-	except:
-		return jsonify({'status': "Error"})"""
-	return "Logs"
 
 @app.route("/api/nums",methods=['GET'])
 def api_nums():
-	"""#list_num = bd.numbers()
 	response = []
 	try:
-		for l in list_num:
-			response.append(dict(id = 0, number=l[1], datetime=l[2]))
+		#Размер базы должен быть меньше 2^32-1;
+		len_n = r.scard("nums")  	#CHECK
+		numbers = r.smembers("nums")
+		for n in numbers:
+			response.append(dict(number=str(n)))
 		return(jsonify(response))
 	except:
-		return jsonify({'status': "Error"})"""
-	return "Nums"
+		data = dict(status = "Error" , datetime=str(datetime.now()), info='Some trouble with BD or sending response.')
+		return jsonify(data)
 
-@app.route("/api/delete")
-def api_delete():
-	try:
-		#bd.delete()
-		return jsonify({'status': "OK"})
-	except:
-		return jsonify({'status': "Error"})
 
 if __name__ == "__main__":
-	"""logging.basicConfig(filename='app.log', filemode='w', format='%(levelname)s - %(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
-	logging.basicConfig(level=logging.DEBUG)
-	#https://webdevblog.ru/logging-v-python/"""
+	logging.basicConfig(filename='app.log', filemode='w', format='%(levelname)s - %(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+	logging.basicConfig(level=logging.DEBUG) #https://webdevblog.ru/logging-v-python/
 	app.run(host='0.0.0.0') 
