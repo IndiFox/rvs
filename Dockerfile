acfec3b77271f51797a1d9947bde887efa315742
indifox <indifox6@gmail.com>
2021-03-06 21:31:22 +0300

    +gunicorn, boot.sh,  processing

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM ubuntu:latest
 LABEL maintainer="Alexander Marchuk"
 RUN apt-get update -y
-RUN apt-get install -y python3-pip python-dev build-essential
+RUN apt-get install -y python3-pip python-dev build-essential gunicorn
 COPY . /app
 WORKDIR /app 
 RUN pip3 install -r requirements.txt
-ENTRYPOINT ["flask"]
-CMD ["run"]
+
+EXPOSE 5000
+RUN chmod +x boot.sh
+ENTRYPOINT ["./boot.sh"]
--- myproject.py
+++ myproject.py
@@ -8,13 +8,22 @@ from dotenv import load_dotenv
 
 app = Flask(__name__)
 
+def get_connection():
+	try:
+		REDIS_SERVICE_SERVICE_HOST = str(os.environ.get('REDIS_SERVICE_SERVICE_HOST')) if os.environ.get("HOST_FROM")== "env" else os.environ.get("HOST_FROM")
+		REDIS_SERVICE_SERVICE_PORT = int(os.environ.get('REDIS_SERVICE_SERVICE_PORT'))
+		r = redis.Redis(host=REDIS_SERVICE_SERVICE_HOST, port=REDIS_SERVICE_SERVICE_PORT)
+		if(r.ping()):
+			return True, r
+	except redis.ConnectionError:
+		logging.error("Connection with DB failed\n")
+		return False, None
+	except:
+		logging.error("Some trubles with DB\n")
+		return False, None
 
-#my_redis - name of docker container
-try:
-	r = redis.Redis(host=str(os.environ.get('REDIS_SERVICE_SERVICE_HOST')) if os.environ.get("HOST_FROM")== "env" else os.environ.get("HOST_FROM"), port=os.environ.get('REDIS_SERVICE_SERVICE_PORT'))
-except redis.ConnectionError:
-	logging.error("Connection with DB failed\n")
-	
+#Установка соединения:
+flag_db, r = get_connection()
 
 @app.route("/")
 def index():
@@ -23,6 +32,7 @@ def index():
 		return render_template('/index.html')
 	except:
 		data = dict(status = "Error" , datetime=str(datetime.now()), info='Some trouble with main page')
+		logging.exception(data)
 		return jsonify(data)
 
 @app.route("/api/n", methods=['POST'])
@@ -32,46 +42,65 @@ def api_number():
 		n = int(data['number'])
 		return api_number_par(n)
 	except:
-		data = dict(status = "Error" , number=0, datetime=str(datetime.now()), info='Some trouble with request or format')
+		data = dict(status = "Error" , number=n, datetime=str(datetime.now()), info='Some trouble with request or format number')
 		logging.exception(data)
 		return jsonify(data)
 
-@app.route("/api/n/<int:n>", methods=['POST'])
 def api_number_par(n):
 	now_time = str(datetime.now())
-	try:
+	global flag_db,r
+	#Переподключение в случае отсутсвия соединения
+	if(flag_db is not None and not flag_db):
+		flag_db,r = get_connection()
+	if(flag_db):
 		if(r.sismember("nums",str(n))):
-			data = dict(status = "Error" , number=n, datetime=now_time, info='Number in DB')
+			data = dict(status = "Error" , number=n, datetime=now_time, info='Number in DB!')
 			logging.error("Number {} in DB.\n".format(n))
 			return jsonify(data)
 		elif(r.sismember("nums", str(n+1))):
-			data = dict(status = "Error" , number=n, datetime=now_time, info='Next number in DB')
+			data = dict(status = "Error" , number=n, datetime=now_time, info='Next number in DB!')
 			logging.error("Next number {} in DB.\n".format(n))
 			return jsonify(data)
 		else:
-			r.sadd("nums",str(n))
-			logging.info("Number {} is added".format(n))
-			data = dict(status = "Ok", number = n+1, datetime = now_time)
-			return jsonify(data)
-	except:
-		data = dict(status = "Error" , number=n, datetime=now_time, info='Somethings happens with writing to BD or exceptions.')
+			#Размер БД Редис должен быть меньше 2^32-1;
+			if(r.scard("nums")<2**32-1):
+				#Запись числа;
+				r.sadd("nums",str(n))
+				logging.info("Number {} is added".format(n))
+				data = dict(status = "Ok", number = n+1, datetime = now_time)
+				return jsonify(data)
+			else:
+				data = dict(status = "Error" , number=n, datetime=now_time, info='DB is full')
+				logging.warning(data)
+				return jsonify(data)
+	else:
+		data = dict(status = "Error" , number=n, datetime=now_time, info='Could not connect to DB')
 		logging.exception(data)
 		return jsonify(data)
 
 @app.route("/api/nums",methods=['GET'])
 def api_nums():
 	response = []
-	try:
-		#Размер базы должен быть меньше 2^32-1;
-		len_n = r.scard("nums")  	#CHECK
-		numbers = r.smembers("nums")
-		for n in numbers:
-			response.append(dict(number=str(n)))
-		return(jsonify(response))
-	except:
-		data = dict(status = "Error" , datetime=str(datetime.now()), info='Some trouble with BD or sending response.')
+	global flag_db,r
+	#Переподключение в случае отсутсвия соединения
+	if(flag_db is not None and not flag_db):
+		flag_db,r = get_connection()
+	if(flag_db):
+		try:
+			#Получение всех чисел из БД и генерация list словарей
+			numbers = r.smembers("nums")
+			for n in numbers:
+				response.append(dict(number=str(n)))
+			return(jsonify(response))
+		except:
+			data = dict(status = "Error" , datetime=str(datetime.now()), info='Could not get data')
+			logging.exception(data)
+			return jsonify(data)
+	else:
+		data = dict(status = "Error" , datetime=str(datetime.now()), info='Could not connect to DB')
 		logging.exception(data)
 		return jsonify(data)
+
 
 
 if __name__ == "__main__":
@@ -85,5 +114,3 @@ if __name__ == "__main__":
 	#logging.basicConfig(filename=os.environ.get('APP_LOG'), filemode='w', format='%(levelname)s - %(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 	#По умолчанию функция запуска берет данные хоста и порта из перменных среды.
 	app.run(host= os.environ.get("FLASK_RUN_HOST"), port = os.environ.get("FLASK_RUN_PORT"))
-
-	#docker exec  -it  f40e61efd9b7 sh
